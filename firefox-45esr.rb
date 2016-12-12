4823ac604ed016461473345c6dfb65ee5900965d
Nathan Fixler <nathan@fixler.org>
2016-12-12 11:50:53 -0600

    Add firefox-45esr formula

---
+++ firefox-45esr.rb
@@ -0,0 +1,16 @@
+cask 'firefox-45esr' do
+  version '45.4.0'
+  sha256 '4e40d588b838709822bd3ccc1edbd13f055df8daf73d1024bcc6c9cbe07ef1e9'
+
+  url "https://download-installer.cdn.mozilla.net/pub/firefox/releases/#{version}esr/mac/en-US/Firefox%20#{version}esr.dmg"
+  name 'Mozilla Firefox'
+  homepage 'https://www.mozilla.org/en-US/firefox/organizations/'
+  license :mpl
+
+  app 'Firefox.app'
+
+  zap delete: [
+                '~/Library/Application Support/Firefox',
+                '~/Library/Caches/Firefox',
+              ]
+end
